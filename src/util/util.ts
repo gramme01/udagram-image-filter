6ac45c2eec5f1ead2da1f01ff99b2d57e4431c7d
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2023-01-07 14:25:30 +0100

    Adds axios to fetch array buffer from image url
    This helps the system accomodate urls containing very large images.

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import fs from "fs";
 import Jimp = require("jimp");
 
@@ -8,18 +9,27 @@ import Jimp = require("jimp");
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
+
+
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
-      await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
-          resolve(__dirname + outpath);
+      axios({
+        method: 'get',
+        url: inputURL,
+        responseType: 'arraybuffer',
+      })
+        .then(async ({ data: imageBuffer }) => {
+          const photo = await Jimp.read(imageBuffer);
+          const outpath =
+            "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+          await photo
+            .resize(256, 256) // resize
+            .quality(60) // set JPEG quality
+            .greyscale() // set greyscale
+            .write(__dirname + outpath, (img) => {
+              resolve(__dirname + outpath);
+            });
         });
     } catch (error) {
       reject(error);
